fa8608bb42ff8a16a507d8452c4eef653514c6ce
wenushkamallikarachchi <w1673746@my.westminster.ac.uk>
2020-05-12 01:27:56 +0530

    new js

--- service.js
+++ service.js
@@ -1,11 +1,9 @@
-const http = require('http');
-  
-const hostname = '127.0.0.1';
-const port = 80;
+var http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(80);
 
+// Console will print the message
+console.log('Server running at http://')
